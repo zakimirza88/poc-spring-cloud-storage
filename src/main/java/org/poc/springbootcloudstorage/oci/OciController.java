a483fa25ef85f659ebc135c6e68ebe9d1071e03c
Zaki M <1521611+zakimirza88@users.noreply.github.com>
2024-08-28 16:17:05 +1000

    Fix download url because file names can contain slashes "/" Polish

--- src/main/java/org/poc/springbootcloudstorage/oci/OciController.java
+++ src/main/java/org/poc/springbootcloudstorage/oci/OciController.java
@@ -1,35 +1,40 @@
 package org.poc.springbootcloudstorage.oci;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.oracle.cloud.spring.storage.OracleStorageResource;
 import com.oracle.cloud.spring.storage.Storage;
+import com.oracle.cloud.spring.storage.StorageObjectMetadata;
 
 @RestController
 public class OciController {
 
+    private final String bucketName;
     private final Storage storage;
 
     public OciController(Storage storage) {
+        this.bucketName = "oci-da-docs";
         this.storage = storage;
     }
 
-    @GetMapping("/files/oci/{fileName}")
-    public ResponseEntity<ByteArrayResource> download(@PathVariable(name = "fileName") String key) throws IOException {
-        String bucketName = "oci-da-docs";
-
+    @GetMapping("/files/oci")
+    public ResponseEntity<ByteArrayResource> download(@RequestParam(name = "fileName") String key) throws IOException {
         OracleStorageResource resource = storage.download(bucketName, key);
         byte[] bytes = resource.getContentAsByteArray();
-
         return ResponseEntity
                 .ok()
                 .headers(contentDisposition(key))
