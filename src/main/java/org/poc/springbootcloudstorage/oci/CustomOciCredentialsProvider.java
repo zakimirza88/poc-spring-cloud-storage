7afe700d8df9a642cee4ec48878051d7a064f275
Zaki M <1521611+zakimirza88@users.noreply.github.com>
2024-08-29 17:03:28 +1000

    Customise config to source connection & authentication property values from properties (instead of from file system ~/.oci/ directory).

--- src/main/java/org/poc/springbootcloudstorage/oci/CustomOciCredentialsProvider.java
+++ src/main/java/org/poc/springbootcloudstorage/oci/CustomOciCredentialsProvider.java
@@ -10,15 +10,28 @@ public final class CustomOciCredentialsProvider extends CredentialsProvider {
 
     private final SimpleAuthenticationDetailsProvider authenticationDetailsProvider;
 
-    public CustomOciCredentialsProvider(CredentialsProperties properties,
-            SimpleAuthenticationDetailsProvider authenticationDetailsProvider) throws IOException {
+    public CustomOciCredentialsProvider(CredentialsProperties properties) throws IOException {
         super(properties);
-        this.authenticationDetailsProvider = authenticationDetailsProvider;
+        this.authenticationDetailsProvider = simpleAuthenticationDetailsProvider(properties);
     }
 
     @Override
     public SimpleAuthenticationDetailsProvider getAuthenticationDetailsProvider() {
         return authenticationDetailsProvider;
+    }
+
+    private SimpleAuthenticationDetailsProvider simpleAuthenticationDetailsProvider(CredentialsProperties properties) {
+        return SimpleAuthenticationDetailsProvider.builder()
+                .userId(properties.getUserId())
+                .tenantId(properties.getTenantId())
+                .fingerprint(properties.getFingerprint())
+                .privateKeySupplier(ociCustomKeySupplier(properties.getPrivateKey()))
+                .passPhrase(properties.getPassPhrase())
+                .build();
+    }
+
+    private OciCustomKeySupplier ociCustomKeySupplier(String privateKey) {
+        return new OciCustomKeySupplier(privateKey);
     }
 
 }
--- src/main/java/org/poc/springbootcloudstorage/oci/OciConfig.java
+++ src/main/java/org/poc/springbootcloudstorage/oci/OciConfig.java
@@ -1,8 +1,19 @@
 package org.poc.springbootcloudstorage.oci;
 
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.oracle.cloud.spring.autoconfigure.core.CredentialsProperties;
+import com.oracle.cloud.spring.autoconfigure.core.CredentialsProvider;
 
 @Configuration
 public class OciConfig {
+
+    @Bean
+    public CredentialsProvider credentialsProvider(CredentialsProperties credentialsProperties) throws IOException {
+        return new CustomOciCredentialsProvider(credentialsProperties);
+    }
 
 }
